1d593046647771c764bff7ab3010ebf9a0989755
josphat-n <josphatnjoroge254@gmail.com>
2024-04-14 01:04:26 +0300

    Input correct credentials to run code.

--- Jenkinsfile
+++ Jenkinsfile
@@ -19,5 +19,10 @@ pipeline {
         sh 'npm test'
       }
     }
+    stage('Run Code') {
+      steps {
+        sh 'npm start'
+      }
+    }
   }
 }
--- _config.js
+++ _config.js
@@ -2,8 +2,12 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://josphat_n:pGezxKp4sGcOLsc8@cluster0.gcfg2ou.mongodb.net/',
+    development: 'mongodb+srv://josphat_n:pGezxKp4sGcOLsc8@cluster0.gcfg2ou.mongodb.net/',
+    test: 'mongodb+srv://josphat_n:pGezxKp4sGcOLsc8@cluster0.gcfg2ou.mongodb.net/',
+
+    // production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    // development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    // test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
